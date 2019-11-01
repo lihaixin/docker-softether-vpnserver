77be290722746b017bef506f9e2523cfc1a85a1a
lihaixin <lihaixin@15099.net>
2019-11-01 10:01:50 +0800

    Update entrypoint.sh
    add mss auto

--- ubuntu16.04/entrypoint.sh
+++ ubuntu16.04/entrypoint.sh
@@ -29,8 +29,10 @@ sleep 2
 /sbin/ip address add 172.16.0.1/24 brd + dev tap_default
 sleep 2
 iptables -t nat -D POSTROUTING -s 172.16.0.0/24 -j MASQUERADE
+iptables -t mangle -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 sleep 2
 iptables -t nat -A POSTROUTING -s 172.16.0.0/24 -j MASQUERADE
+iptables -t mangle -D FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 sleep 2
 
 service dnsmasq  start
