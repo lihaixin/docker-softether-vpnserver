5a0382709534412387f4ca2dbfebc1c441e00def
lihaixin <lihaixin@15099.net>
2019-11-02 17:33:12 +0800

    Update entrypoint.sh

--- ubuntu16.04/entrypoint.sh
+++ ubuntu16.04/entrypoint.sh
@@ -1,24 +1,6 @@
 #!/bin/sh
-
-mkdir -p /usr/local/vpnserver/config
-
-ln -s /usr/local/vpnserver/config/vpn_server.config \
-      /usr/local/vpnserver/vpn_server.config
-
-if [ ! -d "/var/log/vpnserver/security_log" ]; then
-  mkdir -p /var/log/vpnserver/security_log
-  sleep 2
-fi
-
-if [ ! -d "/var/log/vpnserver/packet_log" ]; then
-  mkdir -p /var/log/vpnserver/packet_log
-  sleep 2
-fi
-
-if [ ! -d "/var/log/vpnserver/server_log" ]; then
-  mkdir -p /var/log/vpnserver/server_log
-  sleep 2
-fi
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
 
 /usr/local/vpnserver/vpnserver start
 sleep 2
@@ -29,10 +11,10 @@ sleep 2
 /sbin/ip address add 172.16.0.1/24 brd + dev tap_default
 sleep 2
 iptables -t nat -D POSTROUTING -s 172.16.0.0/24 -j MASQUERADE
-iptables -t mangle -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+# iptables -t mangle -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 sleep 2
 iptables -t nat -A POSTROUTING -s 172.16.0.0/24 -j MASQUERADE
-iptables -t mangle -D FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+# iptables -t mangle -D FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 sleep 2
 
 service dnsmasq  start
